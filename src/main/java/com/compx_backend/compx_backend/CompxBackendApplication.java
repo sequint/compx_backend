dc3e47454d6c2ad56da5c83ac203d801294c7ab1
stevenquintana <steve.e.quint@gmail.com>
2022-08-26 07:12:50 -0700

    Initial commit

---
+++ src/main/java/com/compx_backend/compx_backend/CompxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.compx_backend.compx_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompxBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compx_backend/compx_backend/CompxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compx_backend.compx_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
